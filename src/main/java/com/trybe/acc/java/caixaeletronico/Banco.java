6ca38999e08f1ad0bdc953c5dd58b566c0e759e2
Thauler <thauler98@gmail.com>
2022-09-03 16:14:19 -0300

    feat(Banco): Implements pessoaClienteLogin method
    pessoaClienteLogin: receives two string params and validates then using auxiliaries methods, if the validation of any param returns false the method returns null and if both returns true, the method returns the client with this to params.

--- src/main/java/com/trybe/acc/java/caixaeletronico/Banco.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Banco.java
@@ -1,14 +1,22 @@
 package com.trybe.acc.java.caixaeletronico;
 
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+/**
+ * The type Banco.
+ */
 public class Banco {
-
   private ArrayList<PessoaCliente> pessoasClientes = new ArrayList<>();
   private ArrayList<Conta> contas = new ArrayList<>();
 
+  /**
+   * Gerar numero nova conta string.
+   *
+   * @return the string
+   */
   public String gerarNumeroNovaConta() {
     Random randomize = new Random();
 
@@ -26,6 +34,15 @@ public class Banco {
     return accountID;
   }
 
+  /**
+   * Adicionar pessoa cliente.
+   *
+   * @param name     the name
+   * @param cpf      the cpf
+   * @param password the password
+   *
+   * @return the pessoa cliente
+   */
   public PessoaCliente adicionarPessoaCliente(String name, String cpf, String password) {
 
     PessoaCliente newClient = new PessoaCliente(name, cpf, password);
@@ -35,5 +52,29 @@ public class Banco {
     PessoaCliente lastPersonRegistred = pessoasClientes.get(pessoasClientes.lastIndexOf(newClient));
 
     return lastPersonRegistred;
+  }
+
+  /**
+   * Pessoa cliente login pessoa cliente.
+   *
+   * @param cpf   the cpf
+   * @param senha the senha
+   *
+   * @return the pessoa cliente
+   */
+  public PessoaCliente pessoaClienteLogin(String cpf, String senha) {
+    PessoaCliente clienteLogin = null;
+
+    for (PessoaCliente client: pessoasClientes) {
+      if (!client.validarSenha(senha) || !findCpf(cpf)) {
+        return clienteLogin;
+      }
+      clienteLogin = client;
+    }
+    return clienteLogin;
+  }
+
+  private boolean findCpf(String cpfCheck) {
+    return pessoasClientes.stream().anyMatch(client -> Objects.equals(client.getCpf(), cpfCheck));
   }
 }
