a814521be59c06c57581a2d45597ba3f3fbc9e96
Thauler <thauler98@gmail.com>
2022-09-03 12:52:59 -0300

    feat(Banco): Implements Banco class with two attributes and the method gerarNumeroNovaConta
    The method gerarNumeroNovaConta generates a string with random numbers with the lenght of ten digits

--- src/main/java/com/trybe/acc/java/caixaeletronico/Banco.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Banco.java
@@ -1,5 +1,30 @@
 package com.trybe.acc.java.caixaeletronico;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class Banco {
+
+  private ArrayList<PessoaCliente> pessoaClientes = new ArrayList<>();
+  private ArrayList<Conta> contas = new ArrayList<>();
+
+
+  public String gerarNumeroNovaConta() {
+    Random randomize = new Random();
+
+    ArrayList<String> numArr = new ArrayList<>();
+
+    while (numArr.size() < 10) {
+      numArr.add(Integer.toString(randomize.nextInt(10)));
+    }
+
+    String accountID = numArr
+        .stream()
+        .map(Object::toString)
+        .collect(Collectors.joining(""));
+
+    return accountID;
+  }
 
 }
