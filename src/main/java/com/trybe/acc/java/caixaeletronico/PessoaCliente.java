a01d185a054e73e695f4a54aa015825603879e76
Thauler <thauler98@gmail.com>
2022-09-03 16:02:09 -0300

    feat(PessoaCliente): Implements validarSenha and getCpf methods
    validarSenha: receives a string param and compare to an attribute and returns true to a correct password and false to a wrong one.
    getCpf: getter for the attribute 'cpf'.

--- src/main/java/com/trybe/acc/java/caixaeletronico/PessoaCliente.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/PessoaCliente.java
@@ -1,13 +1,24 @@
 package com.trybe.acc.java.caixaeletronico;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
+/**
+ * The type Pessoa cliente.
+ */
 public class PessoaCliente {
   private String nomeCompleto;
   private String cpf;
   private String senha;
   private ArrayList<Conta> contas = new ArrayList<>();
 
+  /**
+   * Instantiates a new Pessoa cliente.
+   *
+   * @param nomeCompleto the nome completo
+   * @param cpf          the cpf
+   * @param senha        the senha
+   */
   public PessoaCliente(String nomeCompleto, String cpf, String senha) {
     this.nomeCompleto = nomeCompleto;
     this.cpf = cpf;
@@ -16,4 +27,18 @@ public class PessoaCliente {
         + this.nomeCompleto + " com CPF: " + this.cpf + " criada!");
   }
 
+  /**
+   * Validar senha boolean.
+   *
+   * @param senha the senha
+   *
+   * @return the boolean
+   */
+  public boolean validarSenha(String senha) {
+    return Objects.equals(senha, this.senha);
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
 }
