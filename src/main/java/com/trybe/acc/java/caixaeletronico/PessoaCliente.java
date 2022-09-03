c07fcf3bba801993d9f3943e2ec35beab80e42cd
Thauler <thauler98@gmail.com>
2022-09-03 12:57:01 -0300

    feat(PessoaCliente): Implements PessoaCliente class with his attributes and the constructor
    The constructor method initiates three attributes: 'nomeCompleto', 'cpf' and 'senha' and then prints a message on the console.

--- src/main/java/com/trybe/acc/java/caixaeletronico/PessoaCliente.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/PessoaCliente.java
@@ -1,5 +1,19 @@
 package com.trybe.acc.java.caixaeletronico;
 
+import java.util.ArrayList;
+
 public class PessoaCliente {
+  private String nomeCompleto;
+  private String cpf;
+  private String senha;
+  private ArrayList<Conta> contas = new ArrayList<>();
+
+  public PessoaCliente(String nomeCompleto, String cpf, String senha) {
+    this.nomeCompleto = nomeCompleto;
+    this.cpf = cpf;
+    this.senha = senha;
+    System.out.println("Nova pessoa cliente "
+        + this.nomeCompleto + " com CPF: " + this.cpf + " criada!");
+  }
 
 }
