ab2e06e040f46288413814f8218ed89c545d0b05
Thauler <thauler98@gmail.com>
2022-09-03 15:50:45 -0300

    test(PessoaClienteTest): Implements the test for getCpf method
    Tests if 'cpf' of PessoaCliente is equal when the method getter of attribute cpf is called

--- src/test/java/com/trybe/acc/java/caixaeletronico/PessoaClienteTest.java
+++ src/test/java/com/trybe/acc/java/caixaeletronico/PessoaClienteTest.java
@@ -1,28 +1,32 @@
 package com.trybe.acc.java.caixaeletronico;
 
-import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("Teste dos métodos da classe PessoaCliente")
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@DisplayName ("Teste dos métodos da classe PessoaCliente")
 class PessoaClienteTest {
 
   @Test
-  @DisplayName("12 - Testa o construtor da classe Pessoa Cliente.")
+  @DisplayName ("12 - Testa o construtor da classe Pessoa Cliente.")
   void construtorTest() {
     fail("Não implementado");
 
   }
 
   @Test
-  @DisplayName("13 - Testa o método adicionar conta e o método retornar número de contas.")
+  @DisplayName ("13 - Testa o método adicionar conta e o método retornar número de contas.")
   void adicionarContaTestRetornaNumeroDeContasTest() {
     fail("Não implementado");
 
   }
 
   @Test
-  @DisplayName("14 - Testa o método retornar saldo de uma conta específica da pessoa cliente.")
+  @DisplayName ("14 - Testa o método retornar saldo de uma conta específica da pessoa cliente.")
   void retornarSaldoContaEspecificaTest() {
     fail("Não implementado");
 
@@ -30,21 +34,21 @@ class PessoaClienteTest {
 
 
   @Test
-  @DisplayName("15 - Testa o método retornar id de uma conta específica da pessoa cliente.")
+  @DisplayName ("15 - Testa o método retornar id de uma conta específica da pessoa cliente.")
   void retornarIdContaEspecificaTest() {
     fail("Não implementado");
 
   }
 
   @Test
-  @DisplayName("16 - Testa o método retornar o extrato de uma conta específica da pessoa cliente.")
+  @DisplayName ("16 - Testa o método retornar o extrato de uma conta específica da pessoa cliente.")
   void retornarExtratoContaEspecificaTest() {
     fail("Não implementado");
 
   }
 
   @Test
-  @DisplayName("17 - Testa o método adiciona transacao de uma conta específica da pessoa cliente.")
+  @DisplayName ("17 - Testa o método adiciona transacao de uma conta específica da pessoa cliente.")
   void adicionarTransacaoContaEspecificaTest() {
     fail("Não implementado");
 
@@ -58,7 +62,7 @@ class PessoaClienteTest {
   }
 
   @Test
-  @DisplayName("19 - Testa o método retornar resumo contas.")
+  @DisplayName ("19 - Testa o método retornar resumo contas.")
   void retornarResumoContasTest() {
     fail("Não implementado");
 
@@ -66,9 +70,11 @@ class PessoaClienteTest {
   }
 
   @Test
-  @DisplayName("20 - Testa o método Getter do atributo cpf está retornando.")
+  @DisplayName ("20 - Testa o método Getter do atributo cpf está retornando.")
   void getCpfTest() {
-    fail("Não implementado");
+    PessoaCliente client = new PessoaCliente("Thauler", "123.456.789-10", "123456");
+
+    assertEquals("123.456.789-10", client.getCpf());
 
   }
 
