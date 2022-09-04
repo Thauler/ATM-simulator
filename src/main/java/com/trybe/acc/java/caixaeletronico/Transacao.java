07b103e7be36b69bd56f313557bd838e0a7f5013
Thauler <thauler98@gmail.com>
2022-09-04 12:19:34 -0300

    feat(Transacao: Implements Transacao class
    This class has four attributes, in the constructor it is initialized 'quantia', 'descricao' and 'instante' with the method retornarInstante.
    
    getQuantia: the getter of the attribute 'quantia'.
    
    retornarInstante: when is called returns the present date and time.
    
    retornarResumoTransacao: returns a string with the resume of a transaction.

--- src/main/java/com/trybe/acc/java/caixaeletronico/Transacao.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Transacao.java
@@ -1,6 +1,59 @@
 package com.trybe.acc.java.caixaeletronico;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * The type Transacao.
+ */
 public class Transacao {
 
+  private double quantia;
+  private String instante;
+  private String descricao;
+  private Conta conta;
+
+  /**
+   * Instantiates a new Transacao.
+   *
+   * @param quantia   the quantia
+   * @param descricao the descricao
+   */
+  public Transacao(double quantia, String descricao) {
+    this.quantia = quantia;
+    this.descricao = descricao;
+    this.instante = retornarInstante();
+  }
+
+  /**
+   * Gets quantia.
+   *
+   * @return the quantia
+   */
+  public double getQuantia() {
+    return quantia;
+  }
+
+  /**
+   * Retornar instante string.
+   *
+   * @return the string
+   */
+  public String retornarInstante() {
+    String formato = "dd/MM/yyyy HH:mm:ss";
+    String dateAndTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern(formato));
+    return dateAndTime;
+  }
+
+  /**
+   * Retornar resumo transacao string.
+   *
+   * @return the string
+   */
+  public String retornarResumoTransacao() {
+    String extrato = String
+        .format("%s -------- %s: R$ %s +", instante, descricao, quantia);
+    return extrato;
+  }
 }
 
