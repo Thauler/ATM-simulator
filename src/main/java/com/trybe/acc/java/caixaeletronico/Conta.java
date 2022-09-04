6baedee52aca3ccc07cd5b6e92b54db9435fcacf
Thauler <thauler98@gmail.com>
2022-09-04 12:24:44 -0300

    feat(Conta): Implements Conta class
    This class has four attributes, in the constructor it is initialized 'tipoConta', 'pessoaCliente' and 'idConta' with the method gerarNumeroNovaConta form Banco class.

--- src/main/java/com/trybe/acc/java/caixaeletronico/Conta.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Conta.java
@@ -1,6 +1,28 @@
 package com.trybe.acc.java.caixaeletronico;
 
+import java.util.ArrayList;
+
+/**
+ * The type Conta.
+ */
 public class Conta {
+  private String tipoConta;
+  private String idConta;
+  private PessoaCliente pessoaCliente;
+  private ArrayList<Transacao> transacoes = new ArrayList<>();
+
+  /**
+   * Instantiates a new Conta.
+   *
+   * @param tipoConta     the tipo conta
+   * @param pessoaCliente the pessoa cliente
+   * @param banco         the banco
+   */
+  public Conta(String tipoConta, PessoaCliente pessoaCliente, Banco banco) {
+    this.tipoConta = tipoConta;
+    this.pessoaCliente = pessoaCliente;
+    this.idConta = banco.gerarNumeroNovaConta();
+  }
 
 
 }
