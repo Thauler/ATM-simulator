d8bab08d3b1cb10d1f994a09defd51b6a6600cb4
Thauler <thauler98@gmail.com>
2022-09-05 12:37:51 -0300

    test(ContaTest): Implements retornarExtratoTest
    Tests if the method retornarExtrato prints all transactions of the client.

--- src/test/java/com/trybe/acc/java/caixaeletronico/ContaTest.java
+++ src/test/java/com/trybe/acc/java/caixaeletronico/ContaTest.java
@@ -1,15 +1,22 @@
 package com.trybe.acc.java.caixaeletronico;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("Teste da classe Conta")
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@DisplayName ("Teste da classe Conta")
 class ContaTest {
 
   @Test
-  @DisplayName("6 - Testa o construtor da classe conta.")
+  @DisplayName ("6 - Testa o construtor da classe conta.")
   void construtorTest() {
     PessoaCliente client = new PessoaCliente("Thauler", "123.456.789-10", "123456");
     Banco bank = new Banco();
@@ -21,7 +28,7 @@ class ContaTest {
   }
 
   @Test
-  @DisplayName("7 - Testa o método adicionar transação e retornar saldo da conta.")
+  @DisplayName ("7 - Testa o método adicionar transação e retornar saldo da conta.")
   void adicionarTransacaoTestRetornarSaldoTest() {
     PessoaCliente client = new PessoaCliente("Thauler", "123.456.789-10", "123456");
     Banco bank = new Banco();
@@ -35,25 +42,47 @@ class ContaTest {
     account.adicionarTransacao(101, "Saque feito");
     account.adicionarTransacao(100, "Transferência feita");
 
-    assertEquals(-0.9900000000000091, account.retornarSaldo());
+    assertEquals(- 0.9900000000000091, account.retornarSaldo());
   }
 
   @Test
-  @DisplayName("8 - Testa o método retornar resumo está retornando uma string com os valores corretamente.")
+  @DisplayName ("8 - Testa o método retornar resumo está retornando uma string com os valores corretamente.")
   void retornarResumoContaTest() {
     fail("Não implementado");
 
   }
 
   @Test
-  @DisplayName("9 - Testa o método retornar extrato está imprimindo os valores corretamente.")
+  @DisplayName ("9 - Testa o método retornar extrato está imprimindo os valores corretamente.")
   void retornarExtratoTest() {
-    fail("Não implementado");
+    String formato = "dd/MM/yyyy HH:mm:ss";
+    String dateAndTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern(formato));
 
+    OutputStream os = new ByteArrayOutputStream();
+    PrintStream ps = new PrintStream(os);
+    System.setOut(ps);
+
+
+    PessoaCliente client = new PessoaCliente("Thauler", "123.456.789-10", "123456");
+    Banco bank = new Banco();
+    Conta account = new Conta("Corrente", client, bank);
+
+    account.adicionarTransacao(100, "Depósito recebido");
+    account.adicionarTransacao(100, "Depósito recebido");
+    account.adicionarTransacao(100, "Depósito recebido");
+
+    account.retornarExtrato();
+
+    assertEquals("Nova pessoa cliente Thauler com CPF: 123.456.789-10 criada!\n" +
+            dateAndTime + " -------- Depósito recebido: R$ 100.0 +\n" +
+            dateAndTime + " -------- Depósito recebido: R$ 100.0 +\n" +
+            dateAndTime + " -------- Depósito recebido: R$ 100.0 +"
+            + System.getProperty("line.separator"),
+        os.toString());
   }
 
   @Test
-  @DisplayName("10 - Testa o método Getter do atributo idConta está retornando.")
+  @DisplayName ("10 - Testa o método Getter do atributo idConta está retornando.")
   void getIdContaTest() {
     PessoaCliente client = new PessoaCliente("Thauler", "123.456.789-10", "123456");
     Banco bank = new Banco();
@@ -64,7 +93,7 @@ class ContaTest {
   }
 
   @Test
-  @DisplayName("11 - Testa o método método Getter do atributo pessoaCliente está retornando.")
+  @DisplayName ("11 - Testa o método método Getter do atributo pessoaCliente está retornando.")
   void getPessoaClienteTest() {
     PessoaCliente client = new PessoaCliente("Thauler", "123.456.789-10", "123456");
     Banco bank = new Banco();
